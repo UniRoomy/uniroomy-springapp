28924232583687676856a0040fe553f6935178b1
Sahir Ali <sahirali7861@hotmail.com>
2021-05-26 17:24:41 +0100

    circleci test

--- jest.config.ts
+++ jest.config.ts
@@ -116,7 +116,7 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ["/src"],
+  roots: ["<rootDir>/src"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
